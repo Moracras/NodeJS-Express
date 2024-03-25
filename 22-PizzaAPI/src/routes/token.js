525e99dd90b51da9805f1bef42bd980b468f8161
Moracras <tavfikkarademir@gmail.com>
2024-03-26 00:29:30 +0300

    models controllers  routes added

--- 22-PizzaAPI/src/routes/token.js
+++ 22-PizzaAPI/src/routes/token.js
@@ -5,5 +5,22 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 
+// routes:
+
+const token = require('../controllers/token')
+
+// URL: /tokens
+
+router.route('/')
+.get(token.list)
+.post(token.create)
+
+router.route('/:id')
+.get(token.read)
+.put(token.update)
+.patch(token.update)
+.delete(token.delete)
+
+
 /* ------------------------------------------------------- */
 module.exports = router
