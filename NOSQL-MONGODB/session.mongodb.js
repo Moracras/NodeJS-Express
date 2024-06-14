59a211f6a5bbc5a40ef52a31de30ac910ea8ef5b
Moracras <tavfikkarademir@gmail.com>
2024-06-14 18:19:49 +0300

    +

--- NOSQL-MONGODB/session.mongodb.js
+++ NOSQL-MONGODB/session.mongodb.js
@@ -1,4 +1,4 @@
-	/* MongoSH */
+		/* MongoSH */
 	// https://www.mongodb.com/developer/products/mongodb/cheat-sheet/
 	// https://www.mongodb.com/docs/manual/reference/sql-comparison/
 	// https://www.w3schools.com/mongodb/index.php
